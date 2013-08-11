a38036d6010b668c94265b3acc1403a1befe4c53
Mauricio Porto <mauriporto@gmail.com>
2013-08-11 09:18:01 -0300

    Movendo TechCon14.txt p/ este branch e adicionado dois arquivos

---
+++ android/guidedroid/src/com/hp/myidea/guidedroid/base/DirectionListener.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.hp.myidea.guidedroid.base;
+
+/**
+ * @author mauricio
+ *
+ */
+public interface DirectionListener {
+
+    public abstract void onDirectionChanged(float direction);
+}
